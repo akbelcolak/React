15f90589c401875b0dd066181f8c38860e3b36b1
akbel <colakakbel@gmail.com>
2020-03-02 23:22:44 +0100

    could not make ex 8

--- fromscrach/src/Eight.js
+++ fromscrach/src/Eight.js
@@ -1,27 +1,43 @@
-    import React, {useState} from 'react'
-    
-    function Eight({sentence="hello world heyoo"}) {
-        const [items, setItems] = useState([hello])
-        const [index,setIndex] = useState(0)
-        const renderedItems = items.map((item, index) => <li key={index} > {item} </li> )
-        console.log(items)
-        
-    
+import React, { useState } from "react";
 
+function Eight() {
+  const sentence =
+    "hello there, this is my sentence which will be in your app, yehuuu!";
+  let splittedSentenceArray = sentence.split(" ");
 
-        return (
-            <div>
-                <p> {renderedItems} </p>
-                <button onClick={ () => {
-        if (index< sentence.length){
-            items.push(sentence[index])
-            setIndex(index+1)
-            setItems(items)
-        }} } > Previous </button>
-                <button> Next </button>
-            </div>
-        )
-    }
-    
-    export default Eight
-    
+  const [word, setWord] = useState([]);
+  const [index, setIndex] = useState(1)
+
+  
+  //console.log(splittedSentenceArray)
+  //let silicedArray = sentence.split(0,index)
+  //console.log(silicedArray)
+
+  const manual = () => {
+      splittedSentenceArray.map( (w, i) => {
+          if(word[index] === word[i]) {
+              word.push(w)
+              setIndex(i + 1)
+          }
+      })
+  }
+  
+
+  return (
+    <div>
+      <p> {word} </p>
+
+      <button> Previous </button>
+
+      <button
+        onClick={() => {
+          setWord(manual);
+        }}
+      >
+        Next
+      </button>
+    </div>
+  );
+}
+
+export default Eight;
