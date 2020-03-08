96ec5bbcc62ae975479d2abf6e582c46280b0e7a
akbel <colakakbel@gmail.com>
2020-03-08 11:34:07 +0100

    exercise 8 is done!!

--- fromscrach/src/Eight.js
+++ fromscrach/src/Eight.js
@@ -5,19 +5,40 @@ function Eight() {
     "hello there, this is my sentence which will be in your app, yehuuu!";
   let splittedSentenceArray = sentence.split(" ");
 
-  const [word, setWord] = useState([]);
-
-  
+  const [word, setWord] = useState(["hello"]);
+  const [index, setIndex] = useState(1);
+  console.log(word);
 
   return (
     <div>
-      <p> </p>
+      {word.map((i, e) => (
+        <p key={e}> {i} </p>
+      ))}
 
-      <button> Previous </button>
+      <button
+        onClick={() => {
+          let newIndex = index-1 < 0 ? 0 : index-1
+          setWord(splittedSentenceArray.slice(0, newIndex));
+          setIndex(index - 1);
+        }}
+      >
+        {" "}
+        Previous{" "}
+      </button>
 
-      <button> Next </button>
+      <button
+        onClick={() => {
+          let newIndex = index < splittedSentenceArray.length ? index+1 : index
+          setWord(splittedSentenceArray.slice(0, newIndex));
+          setIndex(index + 1);
+        }}
+      >
+        {" "}
+        Next{" "}
+      </button>
     </div>
   );
 }
 
 export default Eight;
+//<button onClick = { ()=> setWord()} > Previous </button>
